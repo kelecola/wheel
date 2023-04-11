3ed752b68c7de390337b2c9f06e43e6b1d7bc440
shenxinyu <kele.shen@trustbe.cn>
2023-04-11 16:18:37 +0800

    fix: 去掉名字

--- src/config/env.production.ts
+++ src/config/env.production.ts
@@ -2,7 +2,7 @@
 module.exports = {
   env: 'production',
   mock: true,
-  title: '生产',
+  title: '',
   baseUrl: 'https://www.xxx.com/', // 正式项目地址
   baseApi: 'https://www.xxx.com/api', // 正式api请求地址
   APPID: 'wxf249da4925081ed9',
