e188a6c9b5697da41917db7ccbc9cfcaeb6b6471
shenxinyu <kele.shen@trustbe.cn>
2023-11-07 18:25:40 +0800

    fix: ci

--- src/config/env.development.ts
+++ src/config/env.development.ts
@@ -2,7 +2,7 @@
 module.exports = {
   env: 'development',
   mock: true,
-  title: '阿里健康',
+  title: '中国野生动物管理专用标识',
   baseUrl: 'http://localhost:9018', // 项目地址
   baseApi: 'https://test.xxx.com/api', // 本地api请求地址,注意：如果你使用了代理，请设置成'/'
   APPID: 'wxf249da4925081ed9',
--- src/config/env.production.ts
+++ src/config/env.production.ts
@@ -2,7 +2,7 @@
 module.exports = {
   env: 'production',
   mock: true,
-  title: '阿里健康',
+  title: '中国野生动物管理专用标识',
   baseUrl: 'https://www.xxx.com/', // 正式项目地址
   baseApi: 'https://www.xxx.com/api', // 正式api请求地址
   APPID: 'wxf249da4925081ed9',
